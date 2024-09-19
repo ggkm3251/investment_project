3d63c39702159fa372c2a3f6017662a24bd13ab6
ggkm3251 <glennkamau2@gmail.com>
2024-09-19 18:00:54 +0300

    Add InvestmentAccountViewSet for managing investment accounts
    - Created  to handle CRUD operations for  model.
    - Uses  for serialization.
    - Restricted access to authenticated users with custom permission .

---
+++ investment/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+class InvestmentAccountViewSet(viewsets.ModelViewSet):
+    queryset = InvestmentAccount.objects.all()
+    serializer_class = InvestmentAccountSerializer
+    permission_classes = [IsAuthenticated, HasAccountPermission]
