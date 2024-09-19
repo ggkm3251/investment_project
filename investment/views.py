72b5f9599dcf04d441993c9436d55e23ec016085
ggkm3251 <glennkamau2@gmail.com>
2024-09-19 21:55:21 +0300

    Add UserCreateView for user registration with profile creation
    - Implemented  using  for user registration.
    - Utilizes  for handling user creation.
    - Custom  method:
      - Validates input data and creates a new  along with an associated .
      - Returns a success response with user data and a confirmation message upon successful creation.
      - Handles validation errors and returns them with a 400 Bad Request status if any issues are found.

--- investment/views.py
+++ investment/views.py
@@ -6,10 +6,12 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import InvestmentAccount, Transaction, UserProfile
-from .serializers import InvestmentAccountSerializer, TransactionSerializer
+from .serializers import InvestmentAccountSerializer, TransactionSerializer, UserCreateSerializer
 from .permissions import HasAccountPermission
 from django_filters.rest_framework import DjangoFilterBackend
 
+from rest_framework import generics
+from rest_framework import status
 
 # Create your views here.
 class InvestmentAccountViewSet(viewsets.ModelViewSet):
@@ -43,3 +45,17 @@ class TransactionViewSet(viewsets.ModelViewSet):
             'transactions': serialized.data,
             'total_balance': total_balance
         })
+    
+
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = UserCreateSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                'user': UserCreateSerializer(user).data,
+                'message': 'User and UserProfile created successfully.'
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
