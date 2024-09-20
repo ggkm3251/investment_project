f994a98b96911522afb4e9f593f043f59d161111
ggkm3251 <glennkamau2@gmail.com>
2024-09-20 13:30:50 +0300

    Add Comments

--- investment/urls.py
+++ investment/urls.py
@@ -2,9 +2,14 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import InvestmentAccountViewSet, TransactionViewSet
 
+# Initialize the default router
 router = DefaultRouter()
+
+# Register viewsets with the router
 router.register(r'accounts', InvestmentAccountViewSet)
 router.register(r'transactions', TransactionViewSet)
+
+
 
 urlpatterns = [
     path('', include(router.urls)),
